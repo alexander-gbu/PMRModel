03b73ed808d3203262c3db191fbe61e1f95fbe30
alexander-gbu <ageldmeyer@g.ucla.edu>
2025-04-02 21:39:39 -0700

    2D doesnt work in basic matlab

--- Matlab/SS2DPDE_LATEST.m
+++ Matlab/SS2DPDE_LATEST.m
@@ -1,2 +1,16 @@
 % Y is going to be the molar fractions. z is going to be axial direction, x is going to be "radial direction"
 
+% this doesnt even work because one of the dimensions has to be time. it cant be z
+% the boundary conditions can be met
+
+
+function [c,f,s] = pdefun(z,x,Y,dYdz)
+
+
+
+
+    c = zeros(length(y),1) + 1;
+	f = zeros(length(y),1);
+	s = -v*dydx + nu*R;
+
+end
