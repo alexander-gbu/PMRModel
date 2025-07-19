a25d1f895586de832c35ab0120d4aa7edf8970bd
alexander-gbu <ageldmeyer@g.ucla.edu>
2025-07-19 01:19:48 -0700

    investigation global current

--- RCE/FeTTPModel.m
+++ RCE/FeTTPModel.m
@@ -39,8 +39,9 @@ delta = 1.58E-05; % boundary layer thickness [m]
 mu = 8.90e-4; % viscosity of water at 25C [Pa.s]
 
 c.T = 298.0;
-c.F = 96485.333;
+c.F = 96485.333; % in C/mol or A*s/mol
 c.R = 8.314459848;
+c.A = 0.0003; %Area of electrode in m2
 
 c.C_Fe3_i = 0.001*1000.0; %initial Fe(III) bulk concentration at t=0 in [mol/m3] units
 c.C_Fe2_i = 0.000*1000.0; %initial Fe(II) bulk concentration at t=0 in [mol/m3] units
@@ -52,10 +53,27 @@ c.D0_Fe2 = 6.7e-10; %Diffusion coefficient of (CO3)2- in water at 25C at infinit
 c.D0_Fe1 = 4.6e-10; %Diffusion coefficient of HCO3- in water at 25C at infinite dilution [m/s]
 c.D0_Fe0 = 5.7e-10; %Diffusion coefficient of HCO3- in water at 25C at infinite dilution [m/s]
 
-c.k0_3_2 =0.00002; % rate constant Fe(III) to Fe(II) (mol/m2/s)                 %0.00002                     higher reaction rates mean steaper slopes
-c.k0_2_1 = 0.00002;% rate constant Fe(II) to Fe(I) (mol/m2/s)
-c.Fe1formation = 0; % Calculation of CO rate of formation at cathode surface (mol/m2/s)
-c.Fe0formation = 0; % Calculation of CO rate of formation at cathode surface (mol/m2/s
+c.k0_3_2 = 10^-30; % rate constant Fe(III) to Fe(II) (m/s)                 %0.00002                     higher reaction rates mean steaper slopes
+c.k0_2_1 = 10^-20;% rate constant Fe(II) to Fe(I) (m/s)
+c.k0_1_0 = 10^-10;
+
+c.E0_3_2=-0.25; %from data
+c.E0_2_1=-1.3;
+c.E0_1_0=-2.0;
+
+function [r3_2, r2_1, r1_0] = reactions(C, E, const)
+    k0_3_2 = const.k0_3_2; % rate constant Fe(III) to Fe(II) (m/s)                 %0.00002                     higher reaction rates mean steaper slopes
+    k0_2_1 = const.k0_2_1;% rate constant Fe(II) to Fe(I) (m/s)
+    k0_1_0 = const.k0_1_0;
+
+    E0_3_2=-0.25; %from data
+    E0_2_1=-1.3;
+    E0_1_0=-2.0;
+
+    r3_2 = k0_3_2*(C(1)*exp(-0.5*(E-E0_3_2)*const.F/const.R/const.T)-C(2)*exp(0.5*(E-E0_3_2)*const.F/const.R/const.T)); %mol/s/m2
+    r2_1 = k0_2_1*(C(2)*exp(-0.5*(E-E0_2_1)*const.F/const.R/const.T)-C(3)*exp(0.5*(E-E0_2_1)*const.F/const.R/const.T));
+    r1_0 = k0_1_0*(C(3)*exp(-0.5*(E-E0_1_0)*const.F/const.R/const.T)-C(4)*exp(0.5*(E-E0_1_0)*const.F/const.R/const.T));
+end
 
 m = 0; 
 xmesh = linspace(0,delta,nmesh);
@@ -74,14 +92,25 @@ current_Fe3 = zeros(1,nmesh);
 current_Fe2 = zeros(1,nmesh);
 current_Fe1 = zeros(1,nmesh);
 current_Fe0 = zeros(1,nmesh);
-for i = 1:nmesh
+for i = 1:nmesh %okay so i dont think this is legal
     dx = xmesh(nmesh) - xmesh(nmesh-1);
-    current_Fe3(i) = -c.F*c.D0_Fe3*(sol(i,nmesh,1)-sol(i,nmesh-1,1))/1000/dx; % reaction is at the right boundary
-    current_Fe2(i) = -c.F*c.D0_Fe2*(sol(i,nmesh,2)-sol(i,nmesh-1,2))/1000/dx;
-    current_Fe1(i) = -c.F*c.D0_Fe1*(sol(i,nmesh,3)-sol(i,nmesh-1,3))/1000/dx;
-    current_Fe0(i) = -c.F*c.D0_Fe0*(sol(i,nmesh,4)-sol(i,nmesh-1,4))/1000/dx;
+    current_Fe3(i) = -c.F*c.A*c.D0_Fe3*(sol(i,nmesh,1)-sol(i,nmesh-1,1))/dx; % reaction is at the right boundary
+    current_Fe2(i) = -c.F*c.A*c.D0_Fe2*(sol(i,nmesh,2)-sol(i,nmesh-1,2))/dx;
+    current_Fe1(i) = -c.F*c.A*c.D0_Fe1*(sol(i,nmesh,3)-sol(i,nmesh-1,3))/dx;
+    current_Fe0(i) = -c.F*c.A*c.D0_Fe0*(sol(i,nmesh,4)-sol(i,nmesh-1,4))/dx;
 end
-global_current = -current_Fe3+current_Fe1+2*current_Fe0;
+global_currentOld = -current_Fe3-current_Fe2-current_Fe1;
+
+for i = 1:nmesh
+    t = tspan(i);
+    [r3_2, r2_1, r1_0] = reactions(sol(i, nmesh-1, :), E(i), c);
+    current_Fe3(i) = 1*c.F*c.A*(r3_2);
+    current_Fe2(i) = 1*c.F*c.A*(r2_1);
+    current_Fe1(i) = 1*c.F*c.A*(r1_0);
+    current_Fe0(i) = 1*c.F*c.A*(0);
+end
+% current_Fe3
+global_current = (current_Fe3-current_Fe2-current_Fe1)*10^8; %-current_Fe0;
 
 % figure(1);
 % surf(xmesh,tspan,u1/1000.0,'edgecolor','none');
@@ -119,10 +148,18 @@ global_current = -current_Fe3+current_Fe1+2*current_Fe0;
 % zlabel('Fe(I)[mol/L]');
 % view(30,20);
 
-% figure(5)
-% plot(tspan,current_Fe3,tspan,current_Fe2,tspan,current_Fe1,tspan,current_Fe0,tspan,global_current,'LineWidth',1.5);
+figure(5)
+plot(tspan,current_Fe3,tspan,current_Fe2,tspan,current_Fe1,tspan,current_Fe0,tspan,global_currentOld,'LineWidth',1.5);
+legend('Fe3', 'Fe2', 'Fe1', 'Fe0', 'global');
 
 figure(6)
+plot(ExpE, ExpI, 'r-', E, global_currentOld, 'b--'); %(floor(nmesh/2):end)
+xlabel('E (V)');
+ylabel('Current (A)');
+title('Experimental vs Model Data');
+legend('Experimental', 'Model');
+
+figure(7)
 plot(ExpE, ExpI, 'r-', E, global_current, 'b--'); %(floor(nmesh/2):end)
 xlabel('E (V)');
 ylabel('Current (A)');
@@ -140,10 +177,6 @@ function u0 = pdeic(x, c)
 end
 
 function [pl,ql,pr,qr] = pdebc(xl,ul,xr,ur,t,c)
-    E0_3_2=-0.25; %from data
-    E0_2_1=-1.3;
-    E0_1_0=-2.0;
-
     if t <= c.half_cycle_time
         E = c.E_start - c.scan_rate*t;
     elseif (t > c.half_cycle_time) && (t <= 2*c.half_cycle_time)
@@ -154,11 +187,10 @@ function [pl,ql,pr,qr] = pdebc(xl,ul,xr,ur,t,c)
         E = c.E_end + c.scan_rate*(t-3*c.half_cycle_time);
     end
 
+    [r3_2, r2_1, r1_0] = reactions(ur, E, c); %formation of Fe2, formation of Fe1, formation of Fe0
+
     ql = [0; 0; 0; 0];
     pl = [ul(1)-c.C_Fe3_i; ul(2)-c.C_Fe2_i; ul(3)-c.C_Fe1_i; ul(4)-c.C_Fe0_i];
     qr = [1; 1; 1; 1];
-    pr = [c.k0_3_2*(ur(1)*exp(-0.5*(E-E0_3_2)*c.F/c.R/c.T)-ur(2)*exp(0.5*(E-E0_3_2)*c.F/c.R/c.T));...
-            -c.k0_3_2*(ur(1)*exp(-0.5*(E-E0_3_2)*c.F/c.R/c.T)-ur(2)*exp(0.5*(E-E0_3_2)*c.F/c.R/c.T))+c.k0_3_2*(ur(2)*exp(-0.5*(E-E0_2_1)*c.F/c.R/c.T)-ur(3)*exp(0.5*(E-E0_2_1)*c.F/c.R/c.T));...
-            -c.k0_3_2*(ur(2)*exp(-0.5*(E-E0_2_1)*c.F/c.R/c.T)-ur(3)*exp(0.5*(E-E0_2_1)*c.F/c.R/c.T))+c.k0_3_2*(ur(3)*exp(-0.5*(E-E0_1_0)*c.F/c.R/c.T)-ur(4)*exp(0.5*(E-E0_1_0)*c.F/c.R/c.T));...
-            -c.k0_3_2*(ur(3)*exp(-0.5*(E-E0_1_0)*c.F/c.R/c.T)-ur(4)*exp(0.5*(E-E0_1_0)*c.F/c.R/c.T))];
+    pr = [r3_2; (-r3_2+r2_1); (-r2_1+r1_0); (-r1_0)]; % -D*dC/dx = r     [m2/s] [mol/m3]/[m] = [mol/m2/s];;; 
 end
