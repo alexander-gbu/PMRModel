9c04e8b360ee4b0010d1543cba80712c1ed4ac1f
alexander-gbu <ageldmeyer@g.ucla.edu>
2025-12-11 16:33:20 -0800

    trying fixes

--- RCE/Momoko/PCO2RFDMPFR.m
+++ RCE/Momoko/PCO2RFDMPFR.m
@@ -10,7 +10,7 @@ close all;
 
 h = 7; %guessed as 7cm
 tend = 50; %minutes
-xmesh = 10000;
+xmesh = 100;
 tmesh = 10000;
 dx = h/xmesh;
 % Stability criterion: CFL condition (v * dt / dx <= 1)
@@ -34,7 +34,7 @@ Hco = 9.5e-5; %mol/cm3 bar Henrys solubility constant for CO
 Hco2 = 3.3e-5; %mol/cm3 bar Henrys solubility constant CO2
 D0co = 1e-1; %guessed
 D0coliq = 1e3;
-K = 100; % = kG*kL/(H*kL+kG) in cm/min mass transfer coeffienct for co in water 0.14 mm/s
+K = 0.01; % = kG*kL/(H*kL+kG) in cm/min mass transfer coeffienct for co in water 0.14 mm/s
 u = F/SAliq; %linear velocity cm/min
 
 C = zeros(length(xspan), length(tspan));
@@ -45,12 +45,16 @@ for j = 1:length(tspan)-1
     C(1,j+1) = C(1,j) + dt * (R - flux);
     C(2,j+1) = C(2,j) + dt * (-u * (C(3+1,j)-C(2,j))/dx + flux);
     C(3:xmesh,j+1) = C(3:xmesh,j) + dt * (-u * (C(3:xmesh,j)-C(2:xmesh-1,j))/dx);
-    C(xmesh+1,j+1) = C(xmesh,j);
     flux = K*(C(1,j+1)-10*C(2,j+1)); %flux into the gas mol/cm3 min
 end
 
 figure()
-surf(tspan, xspan, C, 'edgecolor', 'none');
+surf(tspan, xspan(2:xmesh+1), C(2:xmesh+1, :), 'edgecolor', 'none');
 xlabel('Time (min)');
 ylabel('distance from liquid surface (cm)');
+zlim([0,1])
 title(['CO outlet concentration at ', num2str(P), ' bar'])
+
+figure()
+plot(tspan, C(1,tmesh))
+title(['CO liquid concentration at ', num2str(P), ' bar'])
